c13187652947b7877d86a0c8432d4d15ef07539d
manon jacquin <manon.jacquin@gmail.com>
2019-02-27 11:33:07 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -36,5 +36,8 @@ end
 
 def selective_tweet_shortener(tweets)
   tweets.each do |tweet|
-    if tweet.length > 140
+    if tweet.length > 140 
+    puts word_substituter(tweet)
+    end 
+  end
 end
