7940f201aee1095cb0b40c2a21c9aece9a8d5320
manon jacquin <manon.jacquin@gmail.com>
2019-02-27 10:46:59 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+def dictionary
+  dictionary = {
+   "hello" => "hi",
+    "to" => "2",
+    "too" => "2",
+    "two" => "2",
+    "four" => "4",
+    "for" => "4",
+    "be" =>"b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  tweet_to_a = tweet.split(" ")
+  short_tweet = []
+  tweet_to_a.collect do |word|
+    dictionary.each do |key, value| 
+      if word == key 
+      word = value 
+      end
+    end
+    short_tweet << word
+  end   
+  short_tweet.join(" ")
+end
+
+def bulk_tweet_shortener
+end
